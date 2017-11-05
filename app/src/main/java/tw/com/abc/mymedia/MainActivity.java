a1593075af3a25264704ea8acb90021e13ee627a
Administrator <yadoch@gmail.com>
2017-11-05 13:12:11 +0800

    ListView  部分完成

--- app/src/main/java/tw/com/abc/mymedia/MainActivity.java
+++ app/src/main/java/tw/com/abc/mymedia/MainActivity.java
@@ -1,23 +1,41 @@
 package tw.com.abc.mymedia;
 
+import android.Manifest;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.media.AudioManager;
+import android.media.MediaMetadataRetriever;
 import android.os.Binder;
+import android.os.Environment;
 import android.os.IBinder;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.SeekBar;
+import android.widget.SimpleAdapter;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
     private AudioManager audioManager;
     private SeekBar seekBar;
     private MediaGBService mService;
     boolean isConnect;
+    private ListView listview;
+    private SimpleAdapter adapter;
+    private String[] from = {"title","singer"};
+    private int[] to = {R.id.title, R.id.singer};
+    private LinkedList<HashMap<String,String>> data;
 
     // 自動產生 Override 方法
     private ServiceConnection mCommection=new ServiceConnection() {
@@ -33,15 +51,37 @@ public class MainActivity extends AppCompatActivity {
             isConnect = false;
         }
     };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        init();
-
-
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE,},
+                    0);
+        }else{
+            init();
+        }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            init();
+        }else{
+            finish();
+        }
+    }
+
+
+
     public void test0(View view){
         audioManager.playSoundEffect(AudioManager.FX_KEYPRESS_STANDARD);
 
@@ -54,10 +94,16 @@ public class MainActivity extends AppCompatActivity {
         audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC,AudioManager.ADJUST_LOWER,0);
         seekBar.setProgress(audioManager.getStreamVolume(AudioManager.STREAM_MUSIC));
     }
+    public void test3(View view) {
+        Intent it = new Intent(this,Page2.class);
+        startActivity(it);
+    }
+
+    
 
     private void init(){
         seekBar =(SeekBar) findViewById(R.id.seekbar);
-
+        listview=(ListView) findViewById(R.id.listview);
 
 
         audioManager = (AudioManager)getSystemService(AUDIO_SERVICE);
@@ -83,7 +129,9 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        getMusicList();
     }
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -104,4 +152,55 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
     }
+
+    private void getMusicList() {
+        // 順序為程式架構撰寫順序
+        //3.透過MediaMetadataRetriever 取得檔案資訊
+        MediaMetadataRetriever mmr = new MediaMetadataRetriever();
+
+        //1.取出音樂檔位置-之前有寫展示範例
+        File musicPath =
+                Environment.getExternalStoragePublicDirectory(
+                        Environment.DIRECTORY_MUSIC);
+
+        File[] musics = musicPath.listFiles();
+        //2.利用遞迴逐層取出目錄中的音樂檔案進行處理
+        for (File music : musics) {
+            if (music.isDirectory()) {
+                File[] smusics = music.listFiles();
+                for (File smusic : smusics) {
+                    if (!smusic.isFile()) continue;
+                    // 3.1 取出檔案資訊 子目錄
+                    mmr.setDataSource(smusic.toString());
+
+                    String title = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
+                    String singer = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST);
+
+                    HashMap<String, String> musicInfo = new HashMap<>();
+                    musicInfo.put("file", music.getAbsolutePath());
+                    musicInfo.put(from[0], "sub:" + title);
+                    musicInfo.put(from[1], singer);
+
+                    data.add(musicInfo);
+                }
+            } else if (music.isFile()) {
+                // 3.1 取出檔案資訊 第一層目錄
+                mmr.setDataSource(music.toString());
+
+                String title = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
+                String singer = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST);
+
+
+                HashMap<String, String> musicInfo = new HashMap<>();
+                musicInfo.put("file", music.getAbsolutePath());
+                musicInfo.put(from[0], title);
+                musicInfo.put(from[1], singer);
+
+                data.add(musicInfo);
+            }
+        }
+        adapter=new SimpleAdapter(this,data,R.layout.item,from,to);
+        listview.setAdapter(adapter);
+    }
+    
 }
